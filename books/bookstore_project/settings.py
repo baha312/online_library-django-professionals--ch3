855efaff534229275a626b59edcd107f52b72f95
Baha312 <bahtiyarbolotbekov@gmail.com>
2021-04-30 13:52:46 +0300

    ch5

--- books/bookstore_project/settings.py
+++ books/bookstore_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Local
-    'users.apps.UsersConfig', # new
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 
 ]
 
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
